292a40acab913fc65b2bde6bf76f677f9bcb7971
Calli Bman <calli@bigshell.blackmesa.phreak1n.net>
2019-06-02 16:56:11 +0200

    callibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81, 82, 400, 96 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96 } // maesured 81, 82, 400, 96
 
 /**
  * Default Max Feed Rate (mm/s)
